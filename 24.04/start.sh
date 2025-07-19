571a0d1211f192a35a126881ac161d73d41b1e6b
howzitcal <mrcallumfleming+github@gmail.com>
2025-07-19 07:52:03 +0000

    added welcome message with breakdown

--- 24.04/start.sh
+++ 24.04/start.sh
@@ -2,7 +2,7 @@
 
 # Global Vars
 DOWNLOAD_PATH=$HOME/Downloads/tmp
-OS_VERSION=24.04 LTS
+OS_VERSION=24.04 LTS (KUBUNTU)
 VERSION=0.0.1
 
 # Fetch all the named args
@@ -17,6 +17,30 @@ while [ $# -gt 0 ]; do
 done
 
 clear
+
+echo "----------------------------------------------------"
+echo "Welcome to bunnychow $OS_VERSION (v$VERSION)"
+echo "=> The following will be installed:"
+echo " -> debs: $debs"
+echo " -> flatpaks: $flatpaks"
+if [ -n "$apt_install" ]; then
+  echo "=> the following apt install(s) will be invoked"
+  echo " -> $apt_install"
+fi
+if [ -n "$apt_remove" ]; then
+  echo "=> the following apt remove(s) will be invoked"
+  echo " -> $apt_remove"
+fi
+if [[ $debloat == "yes" ]]; then
+  echo "=> snap packages will be removed"
+fi
+if [[ $neaten == "yes" ]]; then
+  echo "=> the shell will also be neatened"
+fi
+if [[ $theme == "dark" ]]; then
+  echo "=> dark theme will be set"
+fi
+echo "----------------------------------------------------"
 
 sudo apt-get update
 sudo apt-get upgrade -yq
